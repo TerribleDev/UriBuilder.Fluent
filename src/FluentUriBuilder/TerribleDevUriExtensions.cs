480403e3af27e91012b0d52c7982bd82204c8550
Tommy Parnell <tparnell@vistaprint.com>
2016-11-08 13:07:58 -0500

    init

---
+++ src/FluentUriBuilder/TerribleDevUriExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class TerribleDevUriExtensions
+    {
+        public static void WithParameter(this UriBuilder bld, string key, string value)
+        {
+            if(!string.IsNullOrWhiteSpace(bld.Query))
+            {
+                bld.Query += $"&{key}={value}";
+                return;
+            }
+            bld.Query = $"{key}={value}";
+        }
+
+        public static void WithParameter(this UriBuilder bld, string key, IEnumerable<object> values)
+        {
+            var isfirst = string.IsNullOrWhiteSpace(bld.Query);
+            var intitialValue = isfirst ? "?" : "&";
+            var sb = new StringBuilder($"{intitialValue}{key}=");
+            foreach(var value in values)
+            {
+                sb.Append($"{value.ToString()},");
+            }
+            bld.Query = sb.ToString().TrimEnd(',');
+        }
+    }
+}
