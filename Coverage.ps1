2011b935d940d2086d813be6269096dd067158a6
Tommy Parnell <tommy@terribledev.io>
2017-07-05 16:37:42 -0400

    use new proj

--- Coverage.ps1
+++ Coverage.ps1
@@ -3,8 +3,8 @@ if($env:APPVEYOR_REPO_TAG -eq "true")
     "do not publish coverall data on tag builds"
     return
 }
-nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
-nuget install coveralls.net -Version 0.7.0 -OutputDirectory tools
+nuget install OpenCover -Source  https://api.nuget.org/v3/index.json -Version 4.6.519 -OutputDirectory tools
+nuget install coveralls.net -Source  https://api.nuget.org/v3/index.json -Version 0.7.0 -OutputDirectory tools
 
 .\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:" test "".\src\UriBuilder.Fluent.UnitTests"" -f net461" -register:user -filter:"+[UriBuilder*]* -[*Tests]*" -returntargetcode -output:opencover_results.xml
 
