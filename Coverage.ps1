4db92f496504c297f27ba7a7bbc9ac397dd043f4
Tommy Parnell <tommy@terribledev.io>
2017-07-05 17:31:26 -0400

    try a seperate build for coverage

--- Coverage.ps1
+++ Coverage.ps1
@@ -6,6 +6,8 @@ if($env:APPVEYOR_REPO_TAG -eq "true")
 nuget install OpenCover -Source  https://api.nuget.org/v3/index.json -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Source  https://api.nuget.org/v3/index.json -Version 0.7.0 -OutputDirectory tools
 
-.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:" test "".\src\UriBuilder.Fluent.UnitTests\UriBuilder.Fluent.UnitTests.csproj"" -f net461" -register:user -filter:"+[UriBuilder*]* -[*Tests]*" -returntargetcode -output:opencover_results.xml
+dotnet build --configuration Release /p:DebugType=full
+
+.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:" test "".\src\UriBuilder.Fluent.UnitTests\UriBuilder.Fluent.UnitTests.csproj"" -f net461 --no-build" -register:user -filter:"+[UriBuilder*]* -[*Tests]*" -returntargetcode -output:opencover_results.xml
 
 .\tools\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe --opencover -i .\opencover_results.xml
